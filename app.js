7a6a8e05654dca42f7a7b510b5e6471c45006865
Balrog1004 <109793736+Balrog1004@users.noreply.github.com>
2022-08-09 18:08:58 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-import express from "express";
+import express, { query } from "express";
 import mysql from "mysql2/promise";
 import cors from "cors";
 import axios from "axios";
@@ -9,11 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = 4000;
 const pool = mysql.createPool({
   host: "localhost",
-  user: "sbsst",
-  password: "sbs123414",
+  user: "hyoungjun",
+  password: "1173",
   database: "a9",
   waitForConnections: true,
   connectionLimit: 10,
@@ -22,7 +22,6 @@ const pool = mysql.createPool({
 
 const getData = async () => {
   const data = await axios.get("http://localhost:3000/todos");
-  console.log("async await", data);
 };
 
 app.get("/todos/:id/:contentId", async (req, res) => {
@@ -37,21 +36,44 @@ app.get("/todos/:id/:contentId", async (req, res) => {
   const {
     todos: { id, contentId },
   } = data;
-
-  console.log("id", id);
 });
 
 app.get("/todos", async (req, res) => {
   const [rows] = await pool.query("SELECT * FROM todo ORDER BY id DESC");
-
+  //getData()
   res.json(rows);
+});
+
+app.post("/todos", async (req, res) => {
+  const {
+    body: { text },
+  } = req;
+  await pool.query(
+    `
+    INSERT INTO todo
+    SET reg_date = NOW(),
+    perform_date = '2022-08-09 12:12:12',
+    checked = 0,
+    text = ?
+    `,
+    [text]
+  );
+  const [updatedTodos] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    ORDER BY id DESC
+    `
+  );
+
+  res.json(updatedTodos);
 });
 
 app.get("/todos/:id/", async (req, res) => {
   //const id = req.params.id;
   const { id } = req.params;
 
-  const [rows] = await pool.query(
+  const [[todo]] = await pool.query(
     `
   SELECT *
   FROM todo
@@ -59,19 +81,18 @@ app.get("/todos/:id/", async (req, res) => {
   `,
     [id]
   );
-  if (rows.length === 0) {
+  if (!todo) {
     res.status(404).json({
       msg: "not found",
     });
     return;
   }
-
-  res.json(rows[0]);
+  res.json(todo);
 });
 
 app.patch("/todos/:id", async (req, res) => {
   const { id } = req.params;
-  const { perform_date, content } = req.body;
+  const { perform_date, text } = req.body;
 
   const [rows] = await pool.query(
     `
@@ -95,9 +116,9 @@ app.patch("/todos/:id", async (req, res) => {
     return;
   }
 
-  if (!content) {
+  if (!text) {
     res.status(400).json({
-      msg: "content required",
+      msg: "text required",
     });
     return;
   }
@@ -106,15 +127,92 @@ app.patch("/todos/:id", async (req, res) => {
     `
     UPDATE todo
     SET perform_date = ?,
-    content = ?
+    text = ?
     WHERE id = ?
     `,
-    [perform_date, content, id]
+    [perform_date, text, id]
   );
 
-  res.json({
-    msg: `${id}번 할일이 수정되었습니다.`,
-  });
+  const [updatedTodos] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    ORDER BY id DESC
+    `
+  );
+  res.json(updatedTodos);
+});
+
+app.patch("/todos/check/:id", async (req, res) => {
+  const { id } = req.params;
+  const [[rows]] = await pool.query(
+    `
+    SELECT *
+  FROM todo where id = ?
+  `,
+    [id]
+  );
+  if (!rows) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+  await pool.query(
+    `
+  UPDATE todo
+  SET checked = ?
+  WHERE id = ?
+  `,
+
+    [!rows.checked, id]
+  );
+  const [updatedTodos] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    ORDER BY id DESC
+    `
+  );
+  res.json(updatedTodos);
+});
+
+app.patch("/todos/swap/:id", async (req, res) => {
+  const { id } = req.params;
+  const { targetId } = req.body;
+  if (!id) {
+    res.status(400).json({
+      msg: "id required",
+    });
+    return;
+  }
+  if (!targetId) {
+    res.status(400).json({
+      msg: "id targetId",
+    });
+    return;
+  }
+
+  await pool.query(
+    `
+  UPDATE todo a
+  INNER JOIN todo b ON a.id != b.id
+   SET a.reg_date = b.reg_date,
+       a.perform_date = b.perform_date,
+       a.checked = b.checked,
+       a.text = b.text
+  WHERE a.id IN (? , ?) AND b.id IN (? , ?)
+  `,
+    [targetId, id, id, targetId]
+  );
+  const [updatedTodos] = await pool.query(
+    `
+    SELECT *
+    FROM todo
+    ORDER BY id DESC
+    `
+  );
+  res.json(updatedTodos);
 });
 
 app.delete("/todos/:id", async (req, res) => {
